ca1d1f7199f2649a3e37daebee8531c3252420d6
kevin7gif <2632176642@qq.com>
2023-11-24 09:24:37 +0800

    367：有效的完全平方数

---
+++ binary-search/src/main/java/com/kevin/IsPerfectSquare.java
@@ -0,0 +1,45 @@
+package com.kevin;
+
+/**
+ * 367题：有效的完全平方数
+ * @author kevin
+ * @version 1.0
+ * @date 2023-11-24 9:15
+ */
+public class IsPerfectSquare {
+
+    /**
+     * 给你一个正整数 num 。如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+     * 完全平方数 是一个可以写成某个整数的平方的整数。换句话说，它可以写成某个整数和自身的乘积。
+     *
+     * 示例 1：
+     * 输入：num = 16
+     * 输出：true
+     * 解释：返回 true ，因为 4 * 4 = 16 且 4 是一个整数。
+     *
+     * 示例 2：
+     * 输入：num = 14
+     * 输出：false
+     * 解释：返回 false ，因为 3.742 * 3.742 = 14 但 3.742 不是一个整数。
+     */
+
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) {
+            return true;
+        }
+        int left=1;
+        int right=num;
+        while (left <= right){
+            int middle=(left+right)/2;
+            if((long)middle*middle<num){
+                left=middle+1;
+            }else if((long)middle*middle>num)
+            {
+                right=middle-1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
