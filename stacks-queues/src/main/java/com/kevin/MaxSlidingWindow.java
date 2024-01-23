b44732db0987e8379886d66254f0570e9f10b0b6
kevin7gif <2632176642@qq.com>
2024-01-23 15:31:33 +0800

    新增注释

--- stacks-queues/src/main/java/com/kevin/MaxSlidingWindow.java
+++ stacks-queues/src/main/java/com/kevin/MaxSlidingWindow.java
@@ -24,25 +24,34 @@ public class MaxSlidingWindow {
      */
     Deque<Integer> deque = new ArrayDeque<>();
     // 自己实现poll add peek方法
+    // 通过模拟单调队列来实现，单调队列类似  3 --> 2 --> 1 --> 0，最大值永远在队首
+    //弹出元素时，比较当前要弹出的数值是否等于队列出口的数值，如果相等则弹出
+    //同时判断队列当前是否为空
     void poll(int val){
         if(!deque.isEmpty()&&deque.peekFirst()==val){
             deque.pollFirst();
         }
     }
+    //添加元素时，如果要添加的元素大于入口处的元素，就将入口元素弹出
+    //保证队列元素单调递减
+    //比如此时队列元素3,1，2将要入队，比1大，所以1弹出，此时队列：3,2
     void add(int val){
         while (!deque.isEmpty()&&deque.peekLast()<val){
             deque.pollLast();
         }
         deque.add(val);
     }
+    //队列队顶元素始终为最大值
     int peek(){
         return deque.peekFirst();
     }
     public int[] maxSlidingWindow(int[] nums, int k) {
         if(nums.length==1)return nums;
+        //存放结果元素的数组
         int[] res = new int[nums.length-k+1];
         int index = 0;
         MaxSlidingWindow myQueue = new MaxSlidingWindow();
+        //先将前k的元素放入队列
         for(int i=0;i<k; i++){
             myQueue.add(nums[i]);
         }
